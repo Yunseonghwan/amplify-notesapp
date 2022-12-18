93aead1bf7ab61f6ef8ca8c2b5769aa2b3f020b3
Yunseonghwan <sunghwan0208@naver.com>
2022-12-18 16:14:35 +0900

    create Note

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { GraphQLResult } from "@aws-amplify/api-graphql";
 import { API } from "aws-amplify";
-import { useEffect, useReducer } from "react";
+import { ChangeEvent, useEffect, useReducer } from "react";
 import { listTodos } from "./graphql/queries";
 import "antd/dist/reset.css";
-import { List } from "antd";
+import { Button, Input, List } from "antd";
+import { v4 as uuid } from "uuid";
+import { createTodo as CreateTodo } from "./graphql/mutations";
+
+const CLIENT_ID = uuid();
 
 interface IState {
   notes: [];
@@ -23,6 +27,12 @@ const reducer = (state: any, action: any) => {
   switch (action.type) {
     case "SET_NOTES":
       return { ...state, notes: action.notes, loading: false };
+    case "ADD_NOTE":
+      return { ...state, notes: [action.note, ...state.notes] };
+    case "RESET_FORM":
+      return { ...state, form: initalState.form };
+    case "SET_INPUT":
+      return { ...state, form: { ...state.form, [action.name]: action.value } };
     case "ERROR":
       return { ...state, loading: false, error: true };
     default:
@@ -44,6 +54,29 @@ function App() {
     }
   };
 
+  const createNote = async () => {
+    const { form } = state;
+    if (!form.name || !form.description) {
+      return alert("please enter a name and description");
+    }
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() };
+    dispatch({ type: "ADD_NOTE", note });
+    dispatch({ type: "RESET_FORM" });
+    try {
+      await API.graphql({
+        query: CreateTodo,
+        variables: { input: note },
+      });
+      console.log("successfully create note!");
+    } catch (err) {
+      console.log("error:", err);
+    }
+  };
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: "SET_INPUT", name: e.target.name, value: e.target.value });
+  };
+
   useEffect(() => {
     fetchNotes();
   }, []);
@@ -58,6 +91,23 @@ function App() {
 
   return (
     <div style={{ padding: "20px" }}>
+      <Input
+        onChange={onChange}
+        value={state.form.name}
+        placeholder="Note Name"
+        name="name"
+        style={{ marginBottom: "10px" }}
+      />
+      <Input
+        onChange={onChange}
+        value={state.form.description}
+        placeholder="Note description"
+        name="description"
+        style={{ marginBottom: "10px" }}
+      />
+      <Button onClick={createNote} type="primary">
+        Create Note
+      </Button>
       <List
         loading={state.loading}
         dataSource={state.notes}
